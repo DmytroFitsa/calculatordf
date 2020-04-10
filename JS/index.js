2245e92fad863df26b0ff0bba101bfc9223f3399
DmytroFitsa <dmytrofitsa@gmail.com>
2020-04-10 12:53:31 +0300

    calc

---
+++ JS/index.js
@@ -0,0 +1,20 @@
+//cufru
+const display = document.querySelector(".display");
+document
+  .querySelectorAll(".digits button")
+  .forEach((button) => button.addEventListener("click", digitPressed));
+function digitPressed(ev) {
+  display.value += ev.target.innerText;
+}
+//operacii
+document
+  .querySelectorAll(".opers button")
+  .forEach((button) => button.addEventListener("click", operPressed));
+function operPressed(ev) {
+  display.value += ev.target.innerText;
+}
+//button =
+document.querySelector(".eq").addEventListener("click", calculate);
+function calculate() {
+  display.value = eval(display.value);
+}
