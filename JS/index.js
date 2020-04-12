846d6edd4010d0fae486b2fb79ae3fc5e3568ab0
DmytroFitsa <dmytrofitsa@gmail.com>
2020-04-12 14:42:39 +0000

    calcdf

--- JS/index.js
+++ JS/index.js
@@ -1,25 +1,39 @@
 //cufru
 const display = document.querySelector(".display");
 document
-  .querySelectorAll(".numbers button")
+  .querySelectorAll(".digits button")
   .forEach((button) => button.addEventListener("click", digitPressed));
 function digitPressed(ev) {
   display.value += ev.target.innerText;
 }
+
 //operacii
 document
   .querySelectorAll(".opers button")
   .forEach((button) => button.addEventListener("click", operPressed));
 function operPressed(ev) {
-  display.value += ev.target.innerText;
+    if ('+-*/'.includes(display.value[display.value.length - 1])) {
+        display.value = display.value.slice(0, -1);
+        display.value += ev.target.innerText;
+    } else if (display.value[display.value.length - 1] == null) {
+        display.value = null;
+    } else {
+        display.value += ev.target.innerText;
+    }
 }
+
 //button =
 document.querySelector(".eq").addEventListener("click", calculate);
 function calculate() {
-  display.value = eval(display.value);
+    if (display.value.indexOf('/0') !== -1) {
+        display.value = 'DANGER! You divide by 0 :('
+    } else {
+        display.value = parseFloat(eval(display.value).toFixed(3));
+    }
 }
-//button clear
-document.querySelector(".reset").addEventListener("click", clear);
+
+// //button clear
+document.querySelector(".clear").addEventListener("click", clear);
 function clear() {
-  display.value = clear(display.value);
+  display.value = '';
 }
